3c49e31dab7afc33f313d4f7264a13db2d96f94d
Muhammad Robi Munawir <robimunawir1@gmail.com>
2025-04-13 12:12:47 +0700

    add bilangan_terbesar.py on staging

---
+++ bilangan_terbesar.py
@@ -0,0 +1,7 @@
+# Program mencari bilangan terbesar
+angka1 = float(input("Tulis angka pertama: "))
+angka2 = float(input("Tulis angka kedua: "))
+angka3 = float(input("Tulis angka ketiga: "))
+
+terbesar = max(angka1, angka2, angka3)
+print(f"Bilangan terbesar adalah {terbesar}")
